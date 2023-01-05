12280c4110d9fb2c39037e804322974de0c06bf9
yehiel <yehiel.glass@gmail.com>
2023-01-05 13:40:38 +0000

    Added network module2

--- network.tf
+++ network.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "my-resources"
   location = "West Europe"
@@ -6,13 +10,9 @@ resource "azurerm_resource_group" "example" {
 module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = "10.0.0.0/16"
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_enforce_private_link_endpoint_network_policies = {
-    "subnet1" : true
-  }
 
   subnet_service_endpoints = {
     "subnet1" : ["Microsoft.Sql"],
@@ -24,4 +24,6 @@ module "network" {
     environment = "dev"
     costcenter  = "it"
   }
+
+  depends_on = [azurerm_resource_group.example]
 }
