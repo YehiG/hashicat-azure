2fbb691ddec072769d280d20149c26c257b5d4ee
yehiel <yehiel.glass@gmail.com>
2023-01-05 11:40:29 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -107,10 +107,12 @@ resource "azurerm_virtual_machine" "catapp" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
-
+  
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-
+  tags = {
+    Department          = devops
+  }
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
